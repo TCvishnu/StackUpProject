20defeeed1f97a9513d45bd72ce6bf614dadc118
Pranav <pranavkthrideep@gmail.com>
2023-11-10 13:09:35 +0530

    frontend commmit

---
+++ code/contacts/src/App.js
@@ -0,0 +1,16 @@
+import ContactManager from "./ContactManager";
+
+function App() {
+  
+
+  return (
+    <div>
+      <div className="flex flex-row h-screen w-full items-center justify-center">
+      <ContactManager />
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
