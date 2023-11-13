6012ab9176b80ec61fddae08606a4582b9c5fb4a
hr1dyaa <141426942+hr1dyaa@users.noreply.github.com>
2023-11-13 16:41:31 +0530

    Update server2.js

--- code/backend/server2.js
+++ code/backend/server2.js
@@ -57,9 +57,9 @@ app.all('/login', (req, res) => {
                 let parsedData = JSON.parse(existingData);
                 if (parsedData.some(user => user.username === usernameToCheck)){
                     if(parsedData.some(user => user.password === passwordToCheck))
-                        res.json({response : true});
+                        res.json({response : true, username: user.username, data:user.contacts });
                     else
-                        res.json({response : "password"});
+                        res.json({response : "password" });
                 }
                 else    
                     res.json({response : "username"});
@@ -72,7 +72,7 @@ app.all('/contacts', (req, res) => {
     if (req.method === 'POST' || req.method === 'PUT') {
         const newData = req.body;
         const usernameToCheck = newData.username;
-        const contactsToAdd = newData.contacts;
+        const contactToAdd = newData.contacts;
 
         fs.readFile('data.json', 'utf8', (readErr, existingData) => {
             if (readErr) {
@@ -83,19 +83,120 @@ app.all('/contacts', (req, res) => {
                 const userIndex = parsedData.findIndex(user => user.username === usernameToCheck);
 
                 if (userIndex !== -1) {
-                    parsedData[userIndex].contacts = contactsToAdd;
+                    if (!Array.isArray(parsedData[userIndex].contacts)) 
+                        parsedData[userIndex].contacts = [];
+                    
+                    parsedData[userIndex].contacts.push(contactToAdd);
+                    parsedData[userIndex].contacts.sort((a,b)=>{
+                        const nameA=a.name.toUpperCase()
+                        const nameB=b.name.toUpperCase()
+
+                        if(nameA<nameB)
+                            return -1
+                        if (nameA>nameB)
+                            return 1
+                        return 0
+                    })
                     fs.writeFile('data.json', JSON.stringify(parsedData, null, 2), 'utf8', (writeErr) => {
                         if (writeErr) {
                             console.error(writeErr);
                             res.status(500).send('Internal Server Error');
                         } else 
-                            res.json({ response: true });
+                            res.json({ response: true , userData:parsedData[userIndex].contacts });
                     });
                 } else 
                     res.json({ response: "username" });
             }
         });
     }
+});
+
+app.all('/delete', (req, res) => {
+    const usernameToCheck = req.body.username; // Assuming username is in the request body
+    const contactIndexToDelete = req.body.index;
+
+    fs.readFile('data.json', 'utf8', (readErr, existingData) => {
+        if (readErr) {
+            console.error(readErr);
+            res.status(500).send('Internal Server Error');
+            return; // Exit the function to avoid further execution
+        }
+
+        let parsedData;
+
+        try {
+            // Check if existingData is already an object
+            parsedData = typeof existingData === 'object' ? existingData : JSON.parse(existingData);
+        } catch (parseError) {
+            console.error(parseError);
+            res.status(500).send('Error parsing JSON data');
+            return; // Exit the function to avoid further execution
+        }
+
+        const userIndex = parsedData.findIndex(user => user.username === usernameToCheck);
+
+        if (userIndex !== -1) {
+            // Use filter to exclude the contact based on the specified index
+            parsedData[userIndex].contacts = parsedData[userIndex].contacts.filter((contact, index) => index !== contactIndexToDelete);
+
+            fs.writeFile('data.json', JSON.stringify(parsedData, null, 2), 'utf8', (writeErr) => {
+                if (writeErr) {
+                    console.error(writeErr);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.json({ response: true, userData: parsedData[userIndex].contacts });
+                }
+            });
+        } else {
+            res.status(404).json({ response: "User not found" });
+        }
+    });
+});
+
+app.all('/edit', (req, res) => {
+    const usernameToCheck = req.body.username; // Assuming username is in the request body
+    const contactIndexToEdit = req.body.index;
+    const updatedContactData = req.body.updatedContact; // Assuming updatedContact contains the new data
+
+    fs.readFile('data.json', 'utf8', (readErr, existingData) => {
+        if (readErr) {
+            console.error(readErr);
+            res.status(500).send('Internal Server Error');
+            return; // Exit the function to avoid further execution
+        }
+
+        let parsedData;
+        try {
+            // Check if existingData is already an object
+            parsedData = typeof existingData === 'object' ? existingData : JSON.parse(existingData);
+        } catch (parseError) {
+            console.error(parseError);
+            res.status(500).send('Error parsing JSON data');
+            return; // Exit the function to avoid further execution
+        }
+        const userIndex = parsedData.findIndex(user => user.username === usernameToCheck);
+
+        if (userIndex !== -1) {
+
+            if (contactIndexToEdit >= 0 && contactIndexToEdit < parsedData[userIndex].contacts.length) {
+                // Edit the contact based on the specified index
+                parsedData[userIndex].contacts[contactIndexToEdit] = { ...updatedContactData };
+
+                fs.writeFile('data.json', JSON.stringify(parsedData, null, 2), 'utf8', (writeErr) => {
+                    if (writeErr) {
+                        console.error(writeErr);
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        res.json({ response: true, userData: parsedData[userIndex].contacts });
+                    }
+                });
+            } else {
+                res.status(404).json({ response: "Contact index out of bounds" });
+            }
+        } else {
+            res.status(404).json({ response: "User not found" });
+        }
+    });
 });
 
 app.listen(port, () => {
