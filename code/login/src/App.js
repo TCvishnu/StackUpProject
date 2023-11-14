20184d10d6f27893dab8e869fbc07a0e3d996236
Vishnu T C <111764448+TCvishnu@users.noreply.github.com>
2023-11-14 17:15:14 +0530

    Update App.js

--- code/login/src/App.js
+++ code/login/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
-import Form from './Form';
+import Form from './Form2';
 import ContactManager from './ContactManager';
 
 const gotoLogClass = 'loginBtn mb-3 text-white';
